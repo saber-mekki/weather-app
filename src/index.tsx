ec92aa4856cf6c427a00454c700e9da67c0e6887
saber <saber.mekki6@gmail.com>
2023-01-03 01:41:48 +0100

    fix: fix `card time` display in `home` index component

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,70 @@
 import React, { useState, useMemo } from "react";
 import Select from "react-select";
 import countryList from "react-select-country-list";
+import Card from "react-bootstrap/Card";
+import moment from "moment";
+
+import "./index.css"
 
 export function Home() {
-    const [value, setValue] = useState<string>("");
-    const options = useMemo(() => countryList().getData(), []);
+	const [value, setValue] = useState<any>("");
+	const [countryName, setCountryName] = useState<string>("");
+	const [data, setData] = useState<any>();
 
-        const changeHandler = (value:any) => {
-					setValue(value);
-				};
+	const options = useMemo(() => countryList().getData(), []);
+
+	const changeHandler = (value: any) => {
+		setValue(value);
+		setCountryName(value.label);
+	};
+
+	React.useEffect(() => {
+		const fetchData = async () => {
+			await fetch(
+				`${process.env.REACT_APP_WEATHER_URL}/?q=${countryName}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+			)
+				.then((res) => res.json())
+				.then((result) => {
+					setData(result);
+				});
+		};
+		fetchData();
+	}, [countryName]);
 
 	return (
-		   <Select options={options} value={value} onChange={changeHandler} />
-    )
+		<React.Fragment>
+			<Select options={options} value={value} onChange={changeHandler} />
+			<Card className="card__clz">
+				{countryName === "" ? (
+					<Card.Body>
+						<h3 style={{ color: "white" }}>select a country</h3>
+					</Card.Body>
+				) : (
+					<Card.Body>
+						<Card.Title >
+								<p className="time__clz">
+									{moment().format("dddd")},{" "}
+									<span>{moment().format("LL")}</span>
+								</p>
+						</Card.Title>
+						<Card.Subtitle className="mb-2 text-muted">
+							<div >
+								<p className="main__clz">
+									{data.weather && data.weather[0].main}
+								</p>
+							</div>
+						</Card.Subtitle>
+						<div className="temp__clz">
+							<span className="temp-hum pl-4">
+								temp: {data.main && data.main.temp} &deg;C
+							</span>
+							<p className="temp-hum">
+								Humidity: {data.main && data.main.humidity} %
+							</p>
+						</div>
+					</Card.Body>
+				)}
+			</Card>
+		</React.Fragment>
+	);
 }
