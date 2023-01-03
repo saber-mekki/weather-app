6a481aec4e8428fa0c637b6feafedcbe6b8d444b
saber <saber.mekki6@gmail.com>
2023-01-03 01:35:56 +0100

    feat: add `header` folder

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+import "./index.css";
+
+export function Header() {
+    return (
+			<div className="header-container__clz">
+				<h5 className="text-header-container__clz" >
+					Weather-App
+				</h5>
+			</div>
+		);
+}
